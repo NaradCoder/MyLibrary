12c0c838834b5715a3eb8230123438e62250c378
Narad Prajapati <narad.quadtrics@gmail.com>
2022-03-07 15:04:32 +0530

    crop image

---
+++ cropimages/src/main/java/crop/images/ImageMediaAdapter.java
@@ -0,0 +1,93 @@
+package crop.images;
+
+import android.content.Context;
+import android.net.Uri;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class ImageMediaAdapter extends RecyclerView.Adapter<ImageMediaAdapter.MyViewHolder> {
+    public List<String> list;
+    Context context;
+    ClickImageListener clickImageListener;
+
+    public ImageMediaAdapter(List<String> list, Context context, ClickImageListener clickImageListener) {
+        this.list = list;
+        this.context = context;
+        this.clickImageListener = clickImageListener;
+    }
+
+    public interface ClickImageListener {
+        void onClick(String strUri, int position, String type);
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.adapter_media, parent, false);
+
+        MyViewHolder myViewHolder = new MyViewHolder(view);
+        return myViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        Uri uri = Uri.parse(list.get(position));
+        holder.image.setImageURI(uri);
+    }
+
+    public List<String> getList() {
+        return list;
+    }
+
+    public void setItem(String bean) {
+        list.add(bean);
+        notifyDataSetChanged();
+    }
+    public void updateItem(String uri, int position) {
+        list.set(position, uri);
+        notifyItemChanged(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        if (list != null) {
+            return list.size();
+        } else {
+            return 0;
+        }
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView image, crossImage;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            image = itemView.findViewById(R.id.image);
+            crossImage = itemView.findViewById(R.id.delete);
+
+            crossImage.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    clickImageListener.onClick(list.get(getAdapterPosition()), getAdapterPosition(), "remove");
+                }
+            });
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    clickImageListener.onClick(list.get(getAdapterPosition()), getAdapterPosition(), "crop");
+                }
+            });
+        }
+    }
+
+}
